707a3b9460ee93d1c350e55ea04ceb1253fc63ff
Jason Zheng <jazheng@davidson.edu>
2020-02-20 16:57:43 -0800

    working on making buttons light up

--- site/buttons.js
+++ site/buttons.js
@@ -1,15 +1,22 @@
-$('.simon-button.green').on('click', function() {
-  alert('You clicked the green button');
-});
+// let user = [];
+// $('.simon-button.green').on('click', function() {
+//   alert('You clicked the green button');
+//   user.push('green');
+// });
 
-$('.simon-button.blue').on('click', function() {
-  alert('You clicked the blue button');
-});
+// $('.simon-button.blue').on('click', function() {
+//   alert('You clicked the blue button');
+//   user.push('blue');
+// });
 
-$('.simon-button.yellow').on('click', function() {
-  alert('You clicked the yellow button');
-});
+// $('.simon-button.yellow').on('click', function() {
+//   alert('You clicked the yellow button');
+//   user.push('yellow');
+// });
 
-$('.simon-button.red').on('click', function() {
-  alert('You clicked the red button');
-});
+// $('.simon-button.red').on('click', function() {
+//   alert('You clicked the red button');
+//   user.push('red');
+// });
+
+// module.exports = user;
--- site/simon.js
+++ site/simon.js
@@ -1,2 +1,78 @@
 // This file contains the game logic.
 // All the event-listening should happen in buttons.js
+
+// let require
+// let userPattern = require('./buttons.js');
+let pattern = [];
+let htmlButtonNames = ['green','red', 'yellow', 'blue'];
+let red = document.querySelector('.red');
+let blue = document.querySelector('.blue');
+let green = document.querySelector('.green');
+let yellow  = document.querySelector('.yellow');
+let counter = 2;
+
+function changeRed() {
+  red.style.backgroundColor = 'red';
+}
+
+function changeBlue() {
+  blue.style.backgroundColor = 'blue';
+}
+
+function changeGreen() {
+  green.style.backgroundColor = 'green';
+}
+
+function changeYellow() {
+  yellow.style.backgroundColor = 'yellow';
+}
+
+function clearColor() {
+  blue.style.color="#2185D0";
+  red.style.color="#DB2828";
+  yellow.style.color="#FBBD08";
+  green.style.color="#21BA45";
+}
+
+function randomPattern() {
+  for (let i = 0 ; i < counter; i++) {
+    let number = Math.floor(Math.random() * 4);
+    pattern.push(htmlButtonNames[number]);
+  }
+  return pattern;
+}
+
+function changeColors(array) {
+  console.log(array);
+  for (let j = 0; j < array.length; j++) {
+    if (array[j] === 'blue') {
+      let time = setTimeout(changeBlue(), 800);
+      // clearTimeout(time);
+      // clearColor();
+      console.log('hi')
+    }
+    if (array[j] === 'red') {
+      let time = setTimeout(changeRed(), 800);
+      // clearTimeout(time);
+      // clearColor();
+      console.log('hi')
+    }
+    if (array[j] === 'yellow') {
+      let time = setTimeout(changeYellow(), 800);
+      // clearTimeout(time);
+      // clearColor();
+      console.log('hi')
+    }
+    if (array[j] === 'green') {
+      let time = setTimeout(changeGreen(), 800);
+      // clearTimeout(time);
+      // clearColor();
+      console.log('hi')
+    }
+    setTimeout(300000);
+    clearColor();
+  };
+}
+
+
+changeColors(randomPattern());
